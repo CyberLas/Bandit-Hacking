dce464b44807b6701b32ea7a382aeca849c499ef
Carlos Angeles <33170529+CyberLas@users.noreply.github.com>
2023-09-07 09:24:20 -0500

    Delete challenge directory

--- challenge/level_0.sh
+++
@@ -1,9 +0,0 @@
-# Level 0
-
-# The goal of this level is for you to log into the game using SSH. The host to which you need to connect is bandit.labs.overthewire.org, on port 2220. The username is bandit0 and the password is bandit0. Once logged in, go to the Level 1 page to find out how to beat Level 1.
-
-# Ingresar a bash y conectar al SSH 
-ssh -p2220 bandit0@bandit.labs.overthewire.org
-
-# Si pide ingresar una contraseña 
-bandit0
