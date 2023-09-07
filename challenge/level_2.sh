c2bbe9e32f3075e9124f1718284a201906600310
Carlos Angeles <caamnil@gmail.com>
2023-09-07 09:51:45 -0500

    Terminado el nivel 1

---
+++ challenge/level_2.sh
@@ -0,0 +1,15 @@
+# Level 2
+
+# The password for the next level is stored in a file called - located in the home directory
+
+# Luego de haber ingresado al correctamente el SSH del level_1 puedes continuar con el level_2
+
+# Siguiendo los pasos anteriores debemos listar los directorios y directorios ocultos
+ls -la
+
+# Encontraremos un archivo llamado -
+# Con el comando siguiente veremos que es lo que contenia pero como es un guion tendremos que llamarlo diferente para acceder a él.
+cat ./-
+
+# Se encontro un archivo una contraseña que lo usaremos para acceder al siguiente nivel
+rRGizSaX8Mk1RTb1CNQoXTcYZWU6lgzi
